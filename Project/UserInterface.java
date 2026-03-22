b5f50ffe1bd06218f223a5b4f3e9e7101598f21a
ReadyJacob791 <Jacobbohman@msn.com>
2026-03-21 20:45:53 -0400

    commit

--- Project/UserInterface.java
+++ Project/UserInterface.java
@@ -1,19 +1,48 @@
 package Project;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.*;
+import java.util.ArrayList;
+import java.util.List;
 
 // After the login it should initialize the Weighted Graph
 
 public class UserInterface {
-    public void initialize() {
-        // Create the JFrame and set up the GUI components
+    private JFrame frame;
+    private JPanel cardPanel;
+    private CardLayout cardLayout;
 
-        JFrame frame = new JFrame("Bus Station Management System");
+    public static void main(String[] args) {
+        new UserInterface().initialize();
+    }
+
+    public void initialize() {
+        JOptionPane.showMessageDialog(null,
+                "System Warning: Unauthorized access is prohibited. Click OK to proceed.",
+                "Security Alert",
+                JOptionPane.WARNING_MESSAGE);
+
+        // Set size for UI window
+        frame = new JFrame("Route Planner");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 600);
-        frame.setVisible(true);
+        frame.setLocation(null);
 
-        // Display the warning with and ok button to move on to the login page.
+        cardLayout = new CardLayout();
+        cardPanel = new JPanel(cardLayout);
 
-        frame.}
+        cardPanel.add(logInPanel(), "LOGIN");
+        cardPanel.add(dashboardPanel(), "DASHBOARD")
+    }
+
+    private JPanel logInPanel() {
+        JPanel panel = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
+    }
+
+    private JPanel
 }
